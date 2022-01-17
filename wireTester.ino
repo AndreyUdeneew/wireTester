ff04a4f0ae0c33d6fb756b69683828176fcbff8c
Andrey <andrey.udeneew@mail.ru>
2022-01-17 21:05:22 +0300

    v0.7 Pins are zeroed after reading

--- wireTester.ino
+++ wireTester.ino
@@ -1,13 +1,11 @@
 String cmd;
-char num_of_cyckles;
 int num_of_cyckles_int;
-byte testBuffer[70];
 
 void setup() {
   // put your setup code here, to run once:
   uint8_t i;
-  for(i=1;i<=70;i++){
-    pinMode(i,INPUT);
+  for (i = 1; i <= 74; i++) {
+    pinMode(i, INPUT);
   }
   Serial.begin(2000000);
   Serial.setTimeout(100);
@@ -28,15 +26,25 @@ void loop() {
 }
 
 void waiting_4_command() {
+  char num_of_cyckles;
+  String num_of_cyckles_str;
   cmd = "";
   if (Serial.available()) {
     cmd = Serial.readString();
     cmd.trim();
   }
-  if(cmd.substring(0,4)=="GIVE"){
-  num_of_cyckles = cmd[4];
-  num_of_cyckles_int = num_of_cyckles- '0';
-  cmd = "GIVE";
+  if (cmd.substring(0, 4) == "GIVE") {
+    num_of_cyckles = cmd[4];
+    //  num_of_cyckles = char(cmd.substring(5));
+    //  num_of_cyckles_str = String(num_of_cyckles);
+    //  num_of_cyckles_int = num_of_cyckles.toInt();
+    num_of_cyckles_int = num_of_cyckles - '0';
+    //  num_of_cyckles_int = int(num_of_cyckles);
+    //  num_of_cyckles_int = atoi(num_of_cyckles_int);
+
+    //  num_of_cyckles_int = num_of_cyckles- '0';
+
+    cmd = "GIVE";
   }
 }
 
@@ -45,28 +53,41 @@ void I_AM_READY() {
 }
 
 void TESTING() {
-  uint8_t i,j,k,pinVal;
+  byte i, j, k, pinVal, n;
+  byte testBuffer[74];
+  num_of_cyckles_int = byte(num_of_cyckles_int);
   Serial.println("Testing in progress");
-  Serial.println(num_of_cyckles);
-  for(i=0;i<num_of_cyckles;i++){
-    for(j=1;j<=70;j++){
-      pinMode(j,OUTPUT);
-      digitalWrite(j,HIGH);
-      for(k=1;k<=70;k++){
-        if(k!=j){
-          pinVal=digitalRead(k);
-          if(pinVal==1){
-            testBuffer[k]=1; 
+  Serial.print(num_of_cyckles_int, DEC);
+  Serial.println();
+  for (i = 1; i <= 2; i++) {
+    for (j = 1; j <= 74; j++) {
+      pinMode(j, OUTPUT);
+      digitalWrite(j, HIGH);
+      for (k = 1; k <= 74; k++) {
+        if (k != j) {
+          pinVal = digitalRead(k);
+          if (pinVal == 1) {
+            testBuffer[k] = 1;
           }
-          else{
-            testBuffer[k]=0;  
+          else {
+            testBuffer[k] = 0;
           }
           Serial.print(testBuffer[k]);
           Serial.print(',');
+          testBuffer[k]=0;
+        }
+        else {
+          testBuffer[k] = 8;
+          Serial.print(testBuffer[k]);
+          Serial.print(',');
+          testBuffer[k]=0;
         }
       }
-      pinMode(j,INPUT);
+      digitalWrite(j, LOW);
+      pinMode(j, INPUT);
     }
+    Serial.println();
+    //    Serial.print(i);
   }
   Serial.print('\n');
 }
