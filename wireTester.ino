5c75dc5157b2e54453e478b48e7ddbc073587a0b
Andrey <andrey.udeneew@mail.ru>
2022-01-26 21:28:42 +0300

    v0.85 Using of the actual pins was added, necessary delays were added

--- wireTester.ino
+++ wireTester.ino
@@ -1,6 +1,7 @@
 String cmd;
-int numOFpins=74;
+int numOFpins=60;
 int num_of_cyckles_int;
+int actualPins[]={3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,60,61,62,63,64,65,66,67,68,69};
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,7 +15,7 @@ void setup() {
     ;
   }
 }
-
+//Serial.print(actualPins);
 void loop() {
   // put your main code here, to run repeatedly:
   waiting_4_command();
@@ -49,18 +50,23 @@ void TESTING() {
   byte pinVal;
 //  byte testBuffer[numOFpins];
   num_of_cyckles_int = byte(num_of_cyckles_int);
+int  numOFpins = sizeof(actualPins)/sizeof(actualPins[0]);
   Serial.println("Testing in progress");
-  Serial.print(num_of_cyckles_int, DEC);
+  Serial.println(num_of_cyckles_int, DEC);
+  Serial.print(numOFpins, DEC);
   Serial.println();
   for (int i = 1; i <= num_of_cyckles_int; i++) {
     Serial.print("BOD");
-    for (int j = 1; j <= numOFpins; j++) {
-      pinMode(j, OUTPUT);
-      digitalWrite(j, HIGH);
+    for (int j = 1; j <= numOFpins-1; j++) {
+//      pinMode(j, OUTPUT);
+//      digitalWrite(j, HIGH);
+      pinMode(actualPins[j], OUTPUT);
+      digitalWrite(actualPins[j], HIGH);
       delay(1);
-      for (int k = 1; k <= numOFpins; k++) {
+      for (int k = 1; k <= numOFpins-1; k++) {
         if (k != j) {
-          pinVal = digitalRead(k);
+//          pinVal = digitalRead(k);
+            pinVal = digitalRead(actualPins[k]);
           if (pinVal == 1) {
               Serial.print('1');
           }
@@ -72,10 +78,13 @@ void TESTING() {
             Serial.print('8');
         }
       }
-      digitalWrite(j, LOW);
-      pinMode(j, INPUT);
+//      digitalWrite(j, LOW);
+//      pinMode(j, INPUT);
+        digitalWrite(actualPins[j], LOW);
+        pinMode(actualPins[j], INPUT);
     }
     Serial.println("EOD");
   }
   Serial.print('\n');
+  delay(1);
 }
